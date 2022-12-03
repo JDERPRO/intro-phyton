9e837797851223d2d07d47cc645af95fce40f0cc
JDERPRO <104602822+JDERPRO@users.noreply.github.com>
2022-12-03 06:24:57 -0400

    holaaaa sirve porgavor

---
+++ index.py
@@ -0,0 +1,69 @@
+#if 5 > 3:
+    #print("5 es  mayor a 3 ")
+#hola oeqwuez
+#if 3 > 3:
+ #   print("5 es  mayor a 3 ")
+
+
+x = 5
+y = "chachito feliz"
+
+#print(x,y)
+
+var_ = "chanchito"
+
+a, b, c, = "lala","lele","vivi"
+
+#print(a,b,c)
+
+valor1 = valor2 = valor3 = "chancito feliz"
+
+#print (valor1,valor2,valor3)
+
+inicio = "hola"
+final = "mundo"
+concatenado = inicio+final
+
+#print(concatenado)
+
+num_complejo = 1j
+
+#print(num_complejo)
+
+lista = ["hola", "mundo", "hi", "peque"]
+lista2= lista.copy()
+lista.append("chachito triste")
+tupla = ("hola", "mundo", "somos", "tupla")
+listaDeTubpla = list(tupla)
+listaDeTubpla.append("peques")
+#print(lista, lista2.count(3))
+
+#print(len(lista))
+#print(lista)
+
+
+#print(listaDeTubpla)
+
+rango = range(6)
+
+#print(rango)
+
+diccionario ={
+    "nombre" : "Chachito feliz",
+    "raza" : "persa",
+    "edad" : 5,
+}
+
+#print(diccionario)
+#rint(diccionario["nombre"])
+#print(diccionario.get("raza"))
+
+diccionario["nombre"] = "foster"
+
+#print(diccionario)
+
+diccionario.popitem()
+del diccionario["nombre"]
+
+#print(diccionario)
+
